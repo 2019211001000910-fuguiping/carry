2eca4eacda92f4f877a6b9772ef6d8da67421265
付贵平 <2878299041@qq.com>
2021-04-27 19:17:15 +0800

    HW-Week 8 Commit

--- src/main/java/com/fuguiping/week5/demo/LoginServlet.java
+++ src/main/java/com/fuguiping/week5/demo/LoginServlet.java
@@ -1,76 +1,86 @@
 package com.fuguiping.week5.demo;
 
+import com.fuguiping.dao.UserDao;
+import com.fuguiping.model.User;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
 
-@WebInitParam(name="LoginServlet",value="/login")
+//@WebServlet(name = "LoginServlet", value = "/LoginServlet")
+@WebServlet(
+        urlPatterns = {"/login"},
+        loadOnStartup = 4
+)
 public class LoginServlet extends HttpServlet {
     //开始连接数据库
     Connection con=null;
-    String driver;
-    String url;
-    String username;
-    String password;
+    //    String driver;
+//    String url;
+//    String username;
+//    String password;
     @Override
-    public void init() throws ServletException{
-        ServletContext context=this.getServletContext();
-        driver = context.getInitParameter("driver");
-        url = context.getInitParameter("url");
-        username = context.getInitParameter("username");
-        password = context.getInitParameter("password");
+    public void init() throws ServletException {
 
-        try{
-            Class.forName(driver);
-            con = DriverManager.getConnection(url, username, password);
-            System.out.println("进入数据库init()-->"+con);//连接数据库成功
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
+        con = (Connection) getServletContext().getAttribute("con");
     }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getRequestDispatcher("WEB-INF/views/login.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username=request.getParameter("username");
         String password=request.getParameter("password");
 
+        UserDao userDao=new UserDao();
+        try {
+            User user =userDao.findByUsernamePassword(con,username,password);
+            if (user!=null){
+                String RememberMe=request.getParameter("RememberMe");
+                if(RememberMe!=null && RememberMe.equals("1")){
+                    Cookie usernameCookie =new Cookie("cUsername", user.getUsername());
+                    Cookie passwordCookie =new Cookie("cPassword", user.getPassword());
+                    Cookie RememberMeCookie =new Cookie("cRememberMe",RememberMe);
+
+                    usernameCookie.setMaxAge(5);
+                    passwordCookie.setMaxAge(5);
+                    RememberMeCookie.setMaxAge(5);
+
+                    response.addCookie(usernameCookie);
+                    response.addCookie(passwordCookie);
+                    response.addCookie(RememberMeCookie);
+                }
+
+                HttpSession session=request.getSession();
+                System.out.println("session id--->"+session.getId());
+                session.setMaxInactiveInterval(10);
+//                Cookie c=new Cookie("sessionid",""+user.getId());
+//                c.setMaxAge(10*60);
+//                response.addCookie(c);
+
+                session.setAttribute("user",user);
+                request.getRequestDispatcher("WEB-INF/views/userInfo.jsp").forward(request,response);
+            }else {
+                request.setAttribute("message","Username or Password Error !!!");
+                request.getRequestDispatcher("WEB-INF/views/login.jsp").forward(request,response);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
         response.setContentType("text/html");
         PrintWriter writer = response.getWriter();
-        try {
 
-//            HttpSession hs = request.getSession(true);
-//            Statement createDbStatement = con.createStatement();
-
-//            String user = "select * from usertable where username='xxx' and password='xxx'";
-//            ResultSet rs = createDbStatement.executeQuery(user);
-            Statement st = con.createStatement();
-//          定义sql语句
-            String user = "select * from usertable where username=" + "'" + username + "'" + "and password='" + password + "'";
-//          使用resultset接受返回数据
-            ResultSet rs = st.executeQuery(user);
-
-            if (rs.next()){
-                ((PrintWriter) writer).println("Login Success!!!</br>");
-                writer.println("Welcome"+username);
-            }else {
-                writer.println("Username or Password error");
-            }
-
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
     }
 
-    @Override
-    public void destroy() {
-        super.destroy();
-        try {
-            con.close();//当tomcat停止时释放内存
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-    }
+
+
 }
--- src/main/webapp/WEB-INF/views/userInfo.jsp
+++ src/main/webapp/WEB-INF/views/userInfo.jsp
@@ -5,17 +5,29 @@
   Time: 21:56
   To change this template use File | Settings | File Templates.
 --%>
+<%@ page import="com.fuguiping.model.User" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@include file="header.jsp"%>
 <h1>User Info</h1>
+
+<%
+    User u = (User) session.getAttribute("user");
+%>
+
 <table>
     <tr>
-        <td>username:</td><td><%=request.getAttribute("username")%></td>
-        <td>password:</td><td><%=request.getAttribute("password")%></td>
-        <td>email:</td><td><%=request.getAttribute("email")%></td>
-        <td>gender:</td><td><%=request.getAttribute("gender")%></td>
-        <td>birthDate:</td><td><%=request.getAttribute("birthDate")%></td>
-    </tr>
+        <td>username:</td><td><%=u.getUsername()%></td>
+    </tr><tr>
+    <td>password:</td><td><%=u.getPassword()%></td>
+</tr><tr>
+    <td>email:</td><td><%=u.getEmail()%></td>
+</tr><tr>
+    <td>gender:</td><td><%=u.getGender()%></td>
+</tr><tr>
+    <td>birthDate:</td><td><%=u.getBirthDate()%></td>
+</tr>
+
 </table>
+<a href="updateUser">update</a>
 
 <%@include file="footer.jsp"%>
